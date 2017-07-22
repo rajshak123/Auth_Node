82bcf44bf17733d19c00829a61b17acbb52ab49d
rajshak123 <rickysterling9696@gmail.com>
2017-07-21 22:53:57 -0700

    first msg

--- server/models/users.js
+++ server/models/users.js
@@ -1,10 +1,12 @@
 /**
  * Created by raj on 7/14/2017.
+ Model Methods and instance methods
  */
 var mongoose = require('mongoose');
 const validator = require('validator')
 const jwt=require('jsonwebtoken')
 const _=require('lodash')
+const bcryptjs=require('bcryptjs')
 var UserSchema=new mongoose.Schema({
     email: {
         type: String,
@@ -49,6 +51,34 @@ UserSchema.methods.generateAuthToken=function () {
     })
 
 }
+UserSchema.statics.findByToken=function(token){
+var User=this;
+var decoded;
+try{
+    decoded=jwt.verify(token,'abc123');
+}catch(e){
+    return new Promise.reject();
+}
 
+return User.findOne({
+    '_id':decoded._id,
+    'tokens.token':token,
+    'tokens.access':'auth'
+})
+
+}
+UserSchema.pre('save',function(next){
+    var user=this;
+    if(user.isModified('password')){
+        bcryptjs.genSalt(10,(err,salt)=>{
+            bcryptjs.hash(user.password,salt,(err,hash)=>{
+                user.password=hash;
+                next();
+            })
+        })
+    }else{
+    next();
+    }
+})
 var User = mongoose.model('User',UserSchema )
 module.exports={User}
--- server/server.js
+++ server/server.js
@@ -1,51 +1,79 @@
 /**
  * Created by raj on 7/14/2017.
  */
-const express=require('express')
-const _=require('lodash')
+const express = require('express')
+const _ = require('lodash')
 const {ObjectId}=require('mongodb')
 var {mongoose}=require('./db/mongoose')
 var {Todo}=require('./models/todo')
 var {User}=require('./models/users')
 const {SHA256}=require('crypto-js')
-const bodyParser=require('body-parser')
-var app=express()
+const bodyParser = require('body-parser')
+var app = express()
 app.use(bodyParser.json())
 
-const port=process.env.PORT||3000
-app.post('/todos',(req,res)=>{
-  var todo=new Todo({
-    text:req.body["text"]
-  })
-    todo.save().then((docs)=>{
-    res.status(200).json({"inserted":docs})
-}).catch((e)=>res.status(400).json({"msg":"bad request"}))
-})
-app.get('/todos/',(req,res)=>{
-  Todo.find().then((docs)=>{
-    res.status(200).json({docs:docs})
-}).catch((e)=>res.status(400).json({"err":e}))
-})
-app.get('/todos/:id',(req,res)=>{
-  var id=req.params["id"];
-  console.log(id)
-    Todo.findById(req.params.id).then((docs)=>{
-      res.status(200).json({docs:docs})
-    }).catch((e)=>res.status(400).json({"err":e}))
-})
-app.post('/users',(req,res)=>{
-   var body=_.pick(req.body,['email','password'])
-    var user=new User(body);
-   user.save().then((user)=>{
-     return user.generateAuthToken()
-   }).then((token)=>{
+var authenticate=(req,res,next)=>{
+    var token=req.header('x-auth');
+    User.findByToken(token).then((user)=>{
+        if(!user){
+            return Promise.reject()
+        }
+        else
+            req.user=user;
+        next();
+    }).catch((e)=>{
+        res.status(401).send();
+    })
 
-     res.header('x-auth',token).send(user)
-}).catch((e)=>{
-     res.status(400).send(e)
+}
+const port = process.env.PORT || 3000;
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body["text"]
+    })
+    todo.save().then((docs) => {
+    res.status(200).json({"inserted": docs})
+}).
+catch((e) => res.status(400).json({"msg": "bad request"})
+)
+})
+app.get('/todos/', (req,res) => {
+    Todo.find().then((docs) => {
+    res.status(200).json({docs: docs})
+}).
+catch((e) => res.status(400).json({"err": e})
+)
+})
+app.get('/todos/:id', (req, res) => {
+    var id = req.params["id"];
+console.log(id)
+Todo.findById(req.params.id).then((docs) => {
+    res.status(200).json({docs: docs})
+}).
+catch((e) => res.status(400).json({"err": e})
+)
+})
+app.post('/users',(req, res) => {
+    var body = _.pick(req.body, ['email', 'password'])
+    var user = new User(body);
+user.save().then((doc) => {
+    return doc.generateAuthToken()
+}
+).
+then((token) => {
+
+    res.header('x-auth', token).send(user)
+}).
+catch((e) => {
+    res.status(400).send(e)
 })
 
 })
 
-app.listen(port,()=>{console.log(`Server started on ${port}`)})
-module.exports={app}
+app.get('/user/me',authenticate,(req,res)=>{
+    res.send(req.user);
+})
+
+app.listen(port, () => {console.log(`Server started on ${port}`)
+})
+module.exports = {app}
